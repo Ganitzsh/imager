4eda56f14c14caf6b94d1db357ac0e475308b9b5
Julien Ganichot <julien.ganichot@gmail.com>
2019-06-09 22:45:34 +0200

    Adds additional tests

---
+++ delivery/rpcv1/convert_test.go
@@ -0,0 +1,46 @@
+package rpcv1_test
+
+import (
+	"testing"
+
+	"github.com/ganitzsh/12fact/delivery/rpcv1"
+	pb "github.com/ganitzsh/12fact/delivery/rpcv1/proto"
+	"github.com/ganitzsh/12fact/service"
+	"github.com/stretchr/testify/require"
+)
+
+func TestToTransformation(t *testing.T) {
+	s := rpcv1.NewRPCServer(nil)
+	rotate := &pb.RotateImageRequest{
+		Angle:     45.32,
+		ClockWise: true,
+	}
+	transformedRotate := s.ToTransfomation(rotate)
+	require.NotNil(t, transformedRotate)
+	require.EqualValues(t, &service.Rotate{
+		Angle:     45.32,
+		ClockWise: true,
+	}, rotate)
+	blur := &pb.BlurImageRequest{
+		Sigma: 2.0,
+	}
+	transformedBlur := s.ToTransfomation(blur)
+	require.NotNil(t, transformedBlur)
+	require.EqualValues(t, &service.Blur{
+		Sigma: 2.0,
+	}, blur)
+	crop := &pb.CropImageRequest{
+		TopLeftX: 590,
+		TopLeftY: 104,
+		Width:    450,
+		Height:   900,
+	}
+	transformedCrop := s.ToTransfomation(crop)
+	require.NotNil(t, transformedCrop)
+	require.EqualValues(t, &service.Crop{
+		TopLeftX: 590,
+		TopLeftY: 104,
+		Width:    450,
+		Height:   900,
+	}, crop)
+}
